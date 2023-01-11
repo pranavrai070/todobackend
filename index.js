a6fbd5b081ef3c2d92a53212b180783230988ade
pranavrai070 <pranavrai070@gmail.com>
2023-01-11 20:23:57 +0530

    server deployment 2

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
 import Connection from './database/db.js';
 import taskRoutes from './routes/tasks.js';
 import userRoutes from './routes/user.js';
-
 const app = express();
-
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use('/tasks', taskRoutes);
 app.use('/user',userRoutes);
-
-
-const PORT = 5001||process.env.PORT;
-
+const PORT = process.env.PORT||5001;
 Connection();
-
 app.listen(PORT, () => console.log(`Your server is running successfully on PORT ${PORT}`));
