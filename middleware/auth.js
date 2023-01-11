c7ecb112ac96277eb959a557afa6f7ab0b3d83e1
pranavrai070 <pranavrai070@gmail.com>
2023-01-11 21:14:13 +0530

    env update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import jwt from "jsonwebtoken";
 
-const secret = 'test';
-
+const secret = process.env.TOKEN_SECRET;
 const auth = async (req, res, next) => {
   try {
-    // console.log(req.headers);
-    console.log("auth called ");
     if (!req.headers.authorization) return res.status(401).json({message:"Authorization key is Missing"});
     const token = req.headers.authorization.split(" ")[1];
     const isCustomAuth = token.length < 500;
-  
     let decodedData;
-
     if (token && isCustomAuth) {      
       decodedData = jwt.verify(token, secret);
-
       req.userId = decodedData?.id;
     } else {
       decodedData = jwt.decode(token);
-
       req.userId = decodedData?.sub;
     } 
     next();
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
-
 export default auth;
